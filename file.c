b8fd68bdcef0ca7fd9b0d6f768affc4461c0ac18
suker0723 <40301466+suker0723@users.noreply.github.com>
2021-11-02 16:24:17 -0400

    Update file.c

--- file.c
+++ file.c
@@ -29,7 +29,7 @@ task* start_task(task *new_task,int start,int end, my_file *file){
     return new_task;
 }
 
-thread_pool* start_thread_pool(thread_pool *new_thread_pool,int threads_count,task* head,task* tail, int queue_length){
+thread_pool* start_thread_pool(thread_pool *new_thread_pool,int threads_count,task* head, int queue_length){
     if(threads_count==0){
         threads_count=1; // sequentially;
     }else if(head==NULL||queue_length==0){
@@ -38,18 +38,20 @@ thread_pool* start_thread_pool(thread_pool *new_thread_pool,int threads_count,ta
     new_thread_pool= malloc(sizeof(thread_pool));
     new_thread_pool->queue_length=queue_length;
     new_thread_pool->head=head;
-    new_thread_pool->tail=tail;
     new_thread_pool->all_threads= malloc(sizeof(pthread_t)*threads_count);
     new_thread_pool->pending_task=queue_length;
     new_thread_pool->threads_count=threads_count;
+    new_thread_pool->status=0;
     pthread_mutex_init(&(new_thread_pool->mutex),NULL);
     pthread_cond_init(&(new_thread_pool->cond),NULL);
     for(int i=0;i<threads_count;i++){
         pthread_create(&(new_thread_pool->all_threads[0]),NULL,work_thread,new_thread_pool);
         new_thread_pool->threads_count--;
     }
+    new_thread_pool->status=1;
     return new_thread_pool;
 }
+
 
 static void *work_thread(void *new_thread_pool){
     thread_pool *wrapper=(thread_pool*) new_thread_pool;
@@ -61,16 +63,15 @@ static void *work_thread(void *new_thread_pool){
         if(wrapper->head==NULL){
             break;
         }
-        encode(wrapper->head);
+     //   encode(wrapper->head);
+        wrapper->head->completed=true;
 
-        if(wrapper->head==wrapper->tail){
-            wrapper->head=wrapper->tail=NULL;
-        }else{
-            wrapper->head=wrapper->head->next_task;
-        }
+        wrapper->head=wrapper->head->next_task;
+
         wrapper->pending_task--;
         pthread_mutex_unlock(&(wrapper->mutex));
     }
+
     pthread_mutex_unlock(&(wrapper->mutex));
     pthread_exit(NULL);
 }
